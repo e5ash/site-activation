7b8dcd9cf7dfc75ef604e3cffac573e7bffeb4e3
e5ash <e5ash.bro@gmail.com>
2017-06-24 08:00:09 +0300

    100% - 1200px, 30% - responsive

--- template/scripts/scripts.js
+++ template/scripts/scripts.js
@@ -1,16 +1,25 @@
-
-
-
-
-// Select
-$('form').submit(function() {
-	$.ajax({
-	    type: "POST",
-	    url: "/order.php",
-	    data: $(this).serialize()
-	}).done(function() {
-	    popupForm.css('display','none');
-	    popupMessage.css('display','block');
+jQuery(document).ready(function($) {
+	$('.faq__question').click(function() {
+		var icon = $(this).find('.icon-arrow-down'),
+			answer = $(this).next('.faq__answer');
+		icon.toggleClass('rotate');
+		answer.slideToggle(300);
 	});
-	return false;
+	$(window).on('load resize', function() {
+		var windowWidth = $(window).width();
+		if (windowWidth > 1200) {
+			var aside = $('.aside'),
+			asideTop = aside.offset().top;
+			$(window).scroll(function() {
+				var windowScrollY = $(window).scrollTop();
+				if (windowScrollY > asideTop) {
+					aside.css('top', windowScrollY);
+				} else {
+					aside.css('top', '3px');
+				}
+			});
+		}
+		
+	});
+	
 });
